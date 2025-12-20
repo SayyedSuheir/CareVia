43cb463bef93c93b03d78ace5207bba04f558027
suheirelsayyed <sayyed.suheir.auce@gmail.com>
2025-12-20 14:54:28 +0200

    fixing mongodb issue

--- carevia/app/_lib/mongodb.js
+++ carevia/app/_lib/mongodb.js
@@ -2,9 +2,9 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
-  throw new Error('Please define MONGODB_URI in .env.local');
-}
+// if (!MONGODB_URI) {
+//   throw new Error('Please define MONGODB_URI in .env.local');
+// }
 
 let cached = global.mongoose;
 
